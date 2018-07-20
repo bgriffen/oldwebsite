07b113605dd3548ea75e786d3f2b646a7b400346
Brendan Griffen <brendan.f.griffen@gmail.com>
2018-07-20 01:50:16 -0700

    updated navbar

--- static/js/main.js
+++ static/js/main.js
@@ -12,3 +12,11 @@ jQuery(document).ready(function(e) {
 });
 
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
